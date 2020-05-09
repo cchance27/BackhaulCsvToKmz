5567e0633dc65dd236291644f6f1e7133e208490
Chris Chance <chrischance@outlook.com>
2020-05-08 21:23:17 -0400

    Initial working commit

---
+++ Readme.md
@@ -0,0 +1,42 @@
+# generate-link-kmz
+
+This script was designed for internal use but as it seemed so helpful, I decided to share it
+
+## Point-to-Point KMZ
+This file generates a KMZ file to show wireless base station/site locations, along with the wireless point-to-point backhauls between those sites. 
+
+## CSV Input
+
+The input for the script expects 2 csv files. 
+> **sites.csv**: This file is a basic lost of sites and site locations
+
+| Field Name | Description | Required |
+|----|----|----|
+|BBCode|Short-Code Abbreviation of site (eg. XMZI)|required|
+|Site|Full Site Name (eg. XMZ Industries)|required|
+|Latitude|Decimal latitude of the site|required|
+|Longitude|Decimal longitude of the site|required|
+
+> **links.csv**: This file lists all the actual point-to-point backhauls along with details about the links, including the ability to include miscellaneous information that will show in the KMZ when the link is clicked.
+
+| Field Name | Description | Required |
+|----|----|----|
+|From|Site BBCode for start of link|required|
+|To|Site BBCode for end of link|required|
+|Channel Size|MHz this link uses|required|
+_Channel TX|Transmit Channel|required|
+|_Channel RX|Receive Channel|required|
+|_Vendor|Backhaul Vendor|optional|
+|_Anything|You can freely add columns starting with _ and they will be included as an entry in the link description table|optional|
+
+## Execution
+Execution of this script is super simple.
+1.	Copy sites.csv.example to sites.csv and update it with your site information.
+2.	Copy links.csv.example to links.csv and update it with your links.
+3.	Execute generate-link-kmz.ps1 from a powershell terminal
+
+## TODO
+* **Channel Sizes** - Currently only supports 40mhz and 120mhz channel sizes for line thickness
+* **XML Generation Improvement** - Don't like the way I'm doing the XML generation but it works for now and other ways are much more fussy.
+* **Error Checking** - Seriously haven't put any yet
+* **KMZ Compression** - LOL scripts called generate-link-kmz but i don't have it compressing the kml to kmz yet, minor but not done. 
---
+++ generate-link-kmz.ps1
@@ -0,0 +1,152 @@
+﻿$sites = Import-Csv -Delimiter ',' -Path sites.csv
+$links = Import-Csv -Delimiter ',' -Path links.csv
+
+$output = Join-Path -Path (Get-Location) -ChildPath "$(get-date -Format "yyyy-MM-dd") - Backhauls.kml"
+
+Write-Host "Backup Old KML"
+Move-Item -Path $output -Destination $output".backup" -force -ErrorAction SilentlyContinue | Out-Null
+
+$lineWidth120 = 8
+$lineWidth40 = 2
+$channelColors = @{
+	'10735'='da55c1';	
+	'10775'='cef10e';	
+	'10815'='2e22fd';	
+	'10855'='c2bc1b';	
+	'10895'='72e3a9';	
+	'10935'='208cbc';	
+	'10975'='de84cf';	
+	'11015'='cf079f';	
+	'11055'='ffada5';	
+	'11095'='90f3b9';	
+	'11135'='a567f5';	
+	'11175'='2cddf6';
+} 
+
+$kml = @"
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+    <Document>
+    	<Style id="site-visible">
+		    <IconStyle>
+		    	<color>ffffaa00</color>
+		    	<Icon>
+		    		<href>http://maps.google.com/mapfiles/kml/paddle/blu-blank.png</href>
+		    	</Icon>
+		    	<hotSpot x="32" y="1" xunits="pixels" yunits="pixels"/>
+		    </IconStyle>
+		    <LabelStyle>
+		    	<color>ffffaa00</color>
+		    	<scale>1.0</scale>
+		    </LabelStyle>
+		    <LineStyle>
+		    	<color>ffffaa00</color>
+		    </LineStyle>
+	    </Style>
+	    <Style id="site-hidden">
+	    	<IconStyle>
+	    		<color>ffffaa00</color>
+	    		<Icon>
+	    			<href>http://maps.google.com/mapfiles/kml/paddle/blu-blank.png</href>
+	    		</Icon>
+	    		<hotSpot x="32" y="1" xunits="pixels" yunits="pixels"/>
+	    	</IconStyle>
+	    	<LabelStyle>
+	    		<color>00ffaa00</color>
+	    		<scale>1.0</scale>
+	    	</LabelStyle>
+	    	<LineStyle>
+	    		<color>00ffaa00</color>
+	    	</LineStyle>
+	    </Style>
+	    <StyleMap id="site">
+	    	<Pair>
+	    		<key>normal</key>
+	    		<styleUrl>#site-hidden</styleUrl>
+	    	</Pair>
+	    	<Pair>
+	    		<key>highlight</key>
+	    		<styleUrl>#site-visible</styleUrl>
+	    	</Pair>
+	    </StyleMap>
+        <name>UTS-11GHz</name>
+        <Folder>
+            <name>Sites</name>
+            $(
+            	$sites | foreach-object {
+					Write-Host "New Site:" $_.Site
+
+            		'<Placemark>
+            		   <name>{0}</name>
+            		   <styleUrl>#site</styleUrl>
+            		   <Point>
+            		   	<extrude>1</extrude>
+	    			       <altitudeMode>relativeToGround</altitudeMode>
+            		       <coordinates>{1},{2},0</coordinates>
+            		   </Point>
+            		</Placemark>
+            		' -f "[$($_.BBCode)] $($_.Site)", $_.Latitude, $_.Longitude
+             	}
+            )
+        </Folder>
+        <Folder>
+            <name>Backhauls</name>
+            $(
+				$specialFields = ($links | get-member).Name | Where-Object { $_.ToString().StartsWith("_") }
+				
+                $links | foreach-object {
+					Write-Host "New Link: "$fromSite.Site"-"$toSite.Site
+
+                    $link = $_
+                    $fromSite = $($sites | Where-Object { $_.BBCode -eq $link.From })
+					$toSite = $($sites | Where-Object { $_.BBCode -eq $link.To })
+					if ($link."Channel Size" -eq 120) { $linkWidth = $lineWidth120 } else { $linkWidth = $lineWidth40 }
+
+					$lowBandChannel = if ($link."_Channel TX" -lt $link."_Channel RX") { $link."_Channel TX" } else { $link."_Channel RX" }
+					$linkColor = $channelColors[$lowbandChannel]
+
+					$specialData = '<table border=0 width=100%>'
+					$specialData += '<tr><td><b>From</b></td><td>{0}</td></tr>' -f $fromSite.Site
+					$specialData += '<tr><td><b>To</b></td><td>{0}</td></tr>' -f $toSite.Site
+					$specialData += $($specialFields | foreach-object { 
+						'<tr><td><b>{0}</b></td><td>{1}</td></tr>' -f $_.replace("_", ""), $link."$($_)"
+					} | Out-String)
+					$specialData += '</table>'
+					
+                    '<Placemark> 
+			            <name>{0}-{1}</name> 
+			            <description><![CDATA[{7}]]></description>
+			            <LineString>
+			        	    <coordinates>
+			        			{2},{3},0.
+                            	{4},{5},0.
+       		        		</coordinates>
+			            	<altitudeMode>relativeToGround</altitudeMode>
+							<extrude>1</extrude>
+						</LineString>
+						<Style>
+	    					<LineStyle>  
+	    						<color>#ff{11}</color>
+	    						<width>{10}</width>
+	    					</LineStyle> 
+	    				</Style>
+		             </Placemark>
+                    ' -f $link.From, 
+                         $link.To, 
+                         $fromSite.Latitude, 
+                         $fromSite.Longitude, 
+                         $toSite.Latitude, 
+                         $toSite.Longitude,
+                         $link."Channel Size",
+                         $specialData,
+                         $fromSite.Site,
+						 $toSite.Site,
+						 $linkWidth,
+						 $linkColor
+                  })
+        </Folder>
+    </Document>
+</kml>
+"@
+
+$kml | Out-File -Encoding utf8 ($output)
